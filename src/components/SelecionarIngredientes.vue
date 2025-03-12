263ce2877cebb8126e5617a9c293580431129af8
rian-salin <riansalin19@gmail.com>
2025-03-12 13:31:43 -0300

    update -projeto

--- src/components/SelecionarIngredientes.vue
+++ src/components/SelecionarIngredientes.vue
@@ -1,6 +1,7 @@
 <script lang="ts">
 import { obterCategorias} from "../http/index";
-import type ICategoria from '../interfaces/ICategoria';
+import type ICategoria from "../interfaces/ICategoria";
+import  CardCategoria from "../components/CardCategoria.vue"
 
 export default {
     data(){
@@ -10,7 +11,8 @@ export default {
     },
     async created(){
         this.categorias = await obterCategorias();
-    }
+    },
+    components: {CardCategoria} 
 }
 </script>
 
@@ -21,7 +23,7 @@ export default {
 
     <ul class="categorias">
         <li v-for="categoria in categorias" :key="categoria.nome">
-            {{ categoria.nome }}
+            <CardCategoria  :categoria = "categoria" />
         </li>
     </ul>
 
